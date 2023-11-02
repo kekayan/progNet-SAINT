81bd572572462255340675ffe1f493ae50d5281d
kekayan <kekayannanthakumar@gmail.com>
2023-11-02 16:47:27 +1300

    init

---
+++ baselines.py
@@ -0,0 +1,30 @@
+from torch.utils.data import Dataset
+import numpy as np
+
+class DataSetCatCon(Dataset):
+    def __init__(self, X, Y, cat_cols,task='clf',continuous_mean_std=None):
+        
+        cat_cols = list(cat_cols)
+        X_mask =  X['mask'].copy()
+        X = X['data'].copy()
+        con_cols = list(set(np.arange(X.shape[1])) - set(cat_cols))
+        self.X1 = X[:,cat_cols].copy().astype(np.int64) #categorical columns
+        self.X2 = X[:,con_cols].copy().astype(np.float32) #numerical columns
+        self.X1_mask = X_mask[:,cat_cols].copy().astype(np.int64) #categorical columns
+        self.X2_mask = X_mask[:,con_cols].copy().astype(np.int64) #numerical columns
+        if task == 'clf':
+            self.y = Y['data']#.astype(np.float32)
+        else:
+            self.y = Y['data'].astype(np.float32)
+        self.cls = np.zeros_like(self.y,dtype=int)
+        self.cls_mask = np.ones_like(self.y,dtype=int)
+        if continuous_mean_std is not None:
+            mean, std = continuous_mean_std
+            self.X2 = (self.X2 - mean) / std
+
+    def __len__(self):
+        return len(self.y)
+    
+    def __getitem__(self, idx):
+        # X1 has categorical data, X2 has continuous
+        return np.concatenate((self.cls[idx], self.X1[idx])), self.X2[idx],self.y[idx], np.concatenate((self.cls_mask[idx], self.X1_mask[idx])), self.X2_mask[idx]
