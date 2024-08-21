ae6eafaa15502da50b88ac7ea29e3abf6ff50c8f
kekayan <kekayannanthakumar@gmail.com>
2024-08-21 19:45:36 +1200

    added sparsity ex

--- src/augmentations.py
+++ src/augmentations.py
@@ -2,27 +2,32 @@ import torch
 import numpy as np
 
 
-def embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model):
+def embed_data_mask(x_categ, x_cont, cat_mask, con_mask, model):
     """
     This function embeds both categorical and continuous data.
     """
     device = x_cont.device
+    
+    # Move x_categ to the same device as model.categories_offset
+    x_categ = x_categ.to(device)
+    
+    # Ensure model.categories_offset is on the same device as x_categ
+    model.categories_offset = model.categories_offset.to(device)
+    
     x_categ = x_categ + model.categories_offset.type_as(x_categ)
     x_categ_enc = model.embeds(x_categ)
-    n1,n2 = x_cont.shape
+    n1, n2 = x_cont.shape
     _, n3 = x_categ.shape
     if model.cont_embeddings == 'MLP':
-        x_cont_enc = torch.empty(n1,n2, model.dim)
+        x_cont_enc = torch.empty(n1, n2, model.dim, device=device)
         for i in range(model.num_continuous):
-            x_cont_enc[:,i,:] = model.simple_MLP[i](x_cont[:,i])
+            x_cont_enc[:, i, :] = model.simple_MLP[i](x_cont[:, i])
     else:
-        raise Exception('This case should not work!')    
-
+        raise Exception('This case should not work!')
 
     x_cont_enc = x_cont_enc.to(device)
     cat_mask_temp = cat_mask + model.cat_mask_offset.type_as(cat_mask)
     con_mask_temp = con_mask + model.con_mask_offset.type_as(con_mask)
-
 
     cat_mask_temp = model.mask_embeds_cat(cat_mask_temp)
     con_mask_temp = model.mask_embeds_cont(con_mask_temp)
